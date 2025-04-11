bc6fdbe92258676e32a3e90ce8def06668783824
Michael Harrison <mharrison@s3gov.com>
2025-04-11 17:52:56 -0400

    changing the icon images and splash screen

--- scripts/generate_icons.sh
+++ scripts/generate_icons.sh
@@ -4,30 +4,25 @@
 mkdir -p ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset
 mkdir -p ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset
 
-# Set Inkscape path
-INKSCAPE="/opt/homebrew/bin/inkscape"
+# First convert SVGs to high-res PNGs
+magick convert -background none -density 1024 assets/icon.svg ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png
+magick convert -background none -density 1024 assets/splash.svg ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/base.png
 
 # Generate app icons
-$INKSCAPE -w 40 -h 40 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/20@2x.png
-$INKSCAPE -w 60 -h 60 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/20@3x.png
-$INKSCAPE -w 58 -h 58 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/29@2x.png
-$INKSCAPE -w 87 -h 87 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/29@3x.png
-$INKSCAPE -w 80 -h 80 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/40@2x.png
-$INKSCAPE -w 120 -h 120 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/40@3x.png
-$INKSCAPE -w 120 -h 120 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/60@2x.png
-$INKSCAPE -w 180 -h 180 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/60@3x.png
-$INKSCAPE -w 20 -h 20 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ipad20.png
-$INKSCAPE -w 40 -h 40 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ipad20@2x.png
-$INKSCAPE -w 29 -h 29 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ipad29.png
-$INKSCAPE -w 58 -h 58 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ipad29@2x.png
-$INKSCAPE -w 40 -h 40 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ipad40.png
-$INKSCAPE -w 80 -h 80 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ipad40@2x.png
-$INKSCAPE -w 76 -h 76 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ipad76.png
-$INKSCAPE -w 152 -h 152 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ipad76@2x.png
-$INKSCAPE -w 167 -h 167 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ipad83.5@2x.png
-$INKSCAPE -w 1024 -h 1024 assets/icon.svg -o ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/ios-marketing.png
+sips -z 40 40 ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png --out ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/20@2x.png
+sips -z 60 60 ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png --out ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/20@3x.png
+sips -z 58 58 ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png --out ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/29@2x.png
+sips -z 87 87 ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png --out ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/29@3x.png
+sips -z 80 80 ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png --out ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/40@2x.png
+sips -z 120 120 ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png --out ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/40@3x.png
+sips -z 120 120 ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png --out ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/60@2x.png
+sips -z 180 180 ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png --out ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/60@3x.png
 
 # Generate launch images
-$INKSCAPE -w 1242 -h 2436 assets/splash.svg -o ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/launch@3x.png
-$INKSCAPE -w 828 -h 1792 assets/splash.svg -o ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/launch@2x.png
-$INKSCAPE -w 414 -h 896 assets/splash.svg -o ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/launch.png 
+sips -z 1242 2436 ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/base.png --out ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/LaunchImage@3x.png
+sips -z 828 1624 ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/base.png --out ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/LaunchImage@2x.png
+sips -z 414 812 ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/base.png --out ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/LaunchImage.png
+
+# Clean up base images
+rm ios/RN_HealthKit/Images.xcassets/AppIcon.appiconset/base.png
+rm ios/RN_HealthKit/Images.xcassets/LaunchImage.imageset/base.png 
